b9c93b6c388be1b8845dc7684f935483d2d800c1
Jared Fuller <jaredshane@gmail.com>
2017-04-19 10:22:57 -0500

    set up the methods to get all songs and a single song

--- controllers/songCtrl.js
+++ controllers/songCtrl.js
@@ -1,5 +1,28 @@
 'use strict'
 
 // <require a song model>
+const Song = require('../models/song')
+const { bookshelf } = require('../db/database')
 // < use model methods for getting all songs and one song then send the response back with the data>
+
+module.exports.getSongs = (req, res, next) => {
+  Song.getAll()
+  .then( (songs) => {
+    res.json(songs)
+  })
+  .catch( (err) => {
+    next(err)
+  })
+}
+
+module.exports.getSong = ({params: {id}}, res, next) => {
+  Song.getSingleSong(id)
+  .then( (song) => {
+    res.json(song)
+  })
+  .catch( (err) => {
+    next(err)
+  })
+}
+
 // <stretch goal: methods for adding, deleting, editing a song>

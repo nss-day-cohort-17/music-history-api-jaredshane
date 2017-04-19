4ff7fc2a3532ed39f40299108b92955f5f4fd330
Jared Fuller <jaredshane@gmail.com>
2017-04-19 10:22:37 -0500

    set up the app.js file

--- app.js
+++ app.js
@@ -5,7 +5,39 @@ const bodyParser = require('body-parser');
 let app = express();
 
 // <Include the router index file>
+const routes = require('./routes/')
+
+
 // <Setup your routes middleware>
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/api/v1/', routes)
+
 // <catch any undefined routes with a 404 middleware>
-// <Handle any errors that occur in the routing with error handlers defined at the bottom of our 
+app.use( (req, res, next) => {
+  let err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// <Handle any errors that occur in the routing with error handlers defined at the bottom of our
 // middleware stack>
+
+app.use( (err, req, res, next) => {
+  console.log('err', err)
+  res.status(err.status || 500)
+  res.json({
+    message: err.message, 
+    error: err
+  })
+
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(process.env.NODE_ENV)
+  console.log(`Listening on port: ${port}`);
+});
+
+module.exports = app;
